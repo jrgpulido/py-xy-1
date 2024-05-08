9d7956fed1e21e6f4946d3b6de16b2a5bbc417b5
JRG Pulido on MDP <jrgpulido@gmail.com>
2024-05-08 12:12:02 -0500

    minor 2024 update

--- 1intro/2in/input-age.py
+++ 1intro/2in/input-age.py
@@ -3,5 +3,4 @@
 #
 
 age = input('What is your age? ')
-#print("You are half way to %d, wow!",int(age)*2)
-print("You are half way to",age,"wow!",int(age)*2)
+print("You are half way to",int(age)*2,"wow!",age)
